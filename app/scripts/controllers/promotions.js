c588eff762bac2a35648f60e78c89b1ac41739e2
Fergal Moran <fergal.moran@gmail.com>
2014-03-27 14:36:25 +0000

    Initial promotions listing (ng-grid) working

--- app/scripts/controllers/promotions.js
+++ app/scripts/controllers/promotions.js
@@ -2,10 +2,59 @@
 
 angular.module('robotoPromotoApp')
     .controller('PromotionsCtrl', function ($scope, $http) {
-        $http.get('/api/promotions').success(function(promotions) {
-            $scope.gridPromotions = {
-                data: promotions,
-                filterOptions: $scope.filterOptions
-            };
-        });
+        $scope.filterOptions = {
+            filterText: "",
+            useExternalFilter: true
+        };
+        $scope.totalServerItems = 0;
+        $scope.pagingOptions = {
+            pageSizes: [5, 10, 20],
+            pageSize: 5,
+            currentPage: 1
+        }
+
+        $scope.setPagingData = function(data, page, pageSize){
+            var pagedData = data.slice((page-1) * pageSize, page * pageSize);
+            $scope.promotionData = pagedData;
+            $scope.totalServerItems = data.length;
+            if (!$scope.$$phase){
+                $scope.$apply();
+            }
+        };
+        $scope.getPagedDataAsync = function(pageSize, page, searchText){
+            setTimeout(function(){
+                var data;
+                if (searchText){
+                    var ft = searchText.toLowerCase();
+                }else{
+                    $http.get('api/promotions')
+                    .success(function (promotions) {
+                        $scope.setPagingData(promotions, page, pageSize);
+                    });
+                }
+            }, 3000);
+        };
+        $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
+        $scope.$watch('pagingOptions', function(newVal, oldVal){
+            if (newVal != oldVal && newVal.currentPage != oldVal.currentPage){
+                $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
+            }
+        }, true);
+
+        $scope.$watch('filterOptions', function(newVal, oldVal){
+            if (newVal != oldVal){
+                $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
+            }
+        }, true);
+
+        $scope.gridPromotions = {
+            data: 'promotionData',
+            enablePaging: true,
+            showFooter: true,
+            totalServerItems: 'totalServerItems',
+            pagingOptions: $scope.pagingOptions,
+            filterOptions: $scope.filterOptions,
+            columnDefs: [{field:'title', displayName:'Title'}, {field:'created', displayName:'Date Created'}]
+        };
+        
     });
