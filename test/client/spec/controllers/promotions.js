2023a63781a49130d691d5fa031c014f4473bf9d
Fergal Moran <fergal.moran@gmail.com>
2014-03-27 15:53:38 +0000

    Removed awesomeThings boilerplate

--- test/client/spec/controllers/promotions.js
+++ test/client/spec/controllers/promotions.js
@@ -2,21 +2,21 @@
 
 describe('Controller: PromotionsCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('robotoPromotoApp'));
+    // load the controller's module
+    beforeEach(module('robotoPromotoApp'));
 
-  var PromotionsCtrl,
+    var PromotionsCtrl,
     scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    PromotionsCtrl = $controller('PromotionsCtrl', {
-      $scope: scope
-    });
-  }));
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function ($controller, $rootScope) {
+        scope = $rootScope.$new();
+        PromotionsCtrl = $controller('PromotionsCtrl', {
+            $scope: scope
+        });
+    }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
+    it('should attach a list of promotions to the scope', function () {
+        expect(scope.promotions.length).toBe(3);
+    });
 });
