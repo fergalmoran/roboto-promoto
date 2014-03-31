5ee21fbf1e1668fa2a79302b04cd610a4b56864d
Fergal Moran <fergal.moran@gmail.com>
2014-03-31 09:50:58 +0100

    Testing navbar controller

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('robotoPromotoApp')
-  .controller('NavbarCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('NavbarCtrl', function ($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+});
